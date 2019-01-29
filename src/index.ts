b9867f3a993138f715b2cd161287b1fbfa596e53
alextanhongpin <alextan220990@gmail.com>
2019-01-29 22:30:02 +0800

    chore: add request id middleware

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,48 @@
-import Koa from 'koa'
-import koaBody from 'koa-body'
-import http from 'http'
-import {endpoint} from './service/math'
+import Koa, { Context } from 'koa';
+import koaBody from 'koa-body';
+import http from 'http';
+import { endpoint } from './service/math';
+import uuid from 'uuid/v4';
 
 async function main(): Promise<void> {
-	const app = new Koa()
-	app.use(koaBody())
+  const port = process.env.PORT || 3000;
 
-	endpoint(app)
-	// const mathEndpoint = endpoint()
-	// app.use(mathEndpoint.routes())
-	// .use(mathEndpoint.allowedMethods())
+  const app = new Koa();
+  app.use(koaBody());
+  app.use(requestId);
 
-	console.log('listening to port *:3000. press ctrl + c to cancel.')
-	http.createServer(app.callback()).listen(3000)
+  // This must be the first middleware.
+  app.use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      ctx.status = err.status || 500;
+      ctx.body = {
+        error: err.message,
+        // Only log the error code if the request is mutating the states of the
+        // application (POST, PATCH, DELETE etc)
+        // GET methods are usually free of side-effects.
+        ...(ctx.request.method !== 'GET' ? { error_code: ctx.locals.id } : {}),
+      };
+      ctx.app.emit('error', err, ctx);
+    }
+  });
+
+  endpoint(app);
+
+  app.on('error', (err: Error, ctx: Context) => {
+    console.error(err.message, ctx);
+  });
+
+  console.log(`listening to port *:${port}. press ctrl + c to cancel.`);
+  http.createServer(app.callback()).listen(port);
 }
 
-main().catch(console.error)
+main().catch(console.error);
+
+async function requestId(ctx: Context, next: () => Promise<any>) {
+  ctx.locals = ctx.locals || {};
+  ctx.locals.id = uuid();
+  ctx.set('X-Request-ID', ctx.locals.id);
+  await next();
+}
--- src/service/math/index.ts
+++ src/service/math/index.ts
@@ -1,40 +1,40 @@
-import Koa from 'koa'
-import Router from 'koa-router'
+import Koa from 'koa';
+import Router from 'koa-router';
 
-export function endpoint(app: Koa){
-	const router = new Router()
+export function endpoint(app: Koa) {
+  const router = new Router();
 
-	router.get('/sum', getMathBuilder(sum))
-
-	app
-	.use(router.routes())
-	.use(router.allowedMethods())
+  router.get('/sum', getMathBuilder(sum));
+  router.get('/error', async function(_ctx: Koa.Context) {
+    throw new Error('invalid request');
+  });
+  app.use(router.routes()).use(router.allowedMethods());
 }
 
 function getMathBuilder(svc: SumService): Router.IMiddleware {
-	return async function getMath(ctx: Koa.Context) {
-		const {a, b} = ctx.query
-		const req = {a: Number(a), b: Number(b)}
-		const res = svc(req)
-		ctx.body = res
-	}
+  return async function getMath(ctx: Koa.Context) {
+    const { a, b } = ctx.query;
+    const req = { a: Number(a), b: Number(b) };
+    const res = await svc(req);
+    ctx.body = res;
+  };
 }
 
 interface SumRequest {
-	a: number;
-	b: number;
+  a: number;
+  b: number;
 }
 
 interface SumResponse {
-	sum: number;
+  sum: number;
 }
 
 interface SumService {
-	(req: SumRequest): SumResponse;
+  (req: SumRequest): Promise<SumResponse>;
 }
 
-function sum(req: SumRequest): SumResponse {
-	return {
-		sum: req.a + req.b 
-	}
+async function sum(req: SumRequest): Promise<SumResponse> {
+  return {
+    sum: req.a + req.b,
+  };
 }
